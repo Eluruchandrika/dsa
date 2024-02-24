b80b55405efb536f21ac4182b5ebcaeb438981d6
Eluruchandrika <chandrikaeluru2005@gmail.com>
2024-02-24 15:25:44 +0530

    tower of honoi using stack

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,37 @@
-st1.pop()
-st1.print_stack()
-print("top ele of stack:", st.top())
+def create_stack():
+    stack=[]
+    return stack    
+def push(stack,data):
+    stack.append(data)
+def pop(stack):
+    if not isempty(stack):
+      return stack.pop()
+def isempty(stack):
+    return len(stack)==0
+    
+def tower_of_honoi(n,sou,aux,des):
+    sou_stack=create_stack()
+    aux_stack=create_stack()
+    des_stack=create_stack()
+    for i in range(n,0,-1):
+      push(sou_stack,i)
+    total_moves=(2**n)-1
+    if n%2==0:
+      aux,des=des,aux
+    for move in range(1,total_moves+1):
+      if move%3==1:
+        move_disk(sou_stack,des_stack,sou,des)
+      elif move%3==2:
+        move_disk(sou_stack,aux_stack,sou,aux)
+      elif move%3==0:
+        move_disk(aux_stack,des_stack,aux,des)
+        
+def move_disk(sou_st,des_st,sou,des):
+  disk=pop(sou_st)
+  push(des_st,disk)
+  print("move disk",disk,"from",sou,"to",des)
+  
+n=3
+tower_of_honoi(n,'a','b','c')
+      
+  
